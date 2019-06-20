b5c69fb97bfc0cb10dfe57624b15a42dc783dd7d
robertcarlsson <robert.carlsson1987@gmail.com>
2019-06-20 15:33:26 +0200

    Refactoring v.1

--- server/server.py
+++ server/server.py
@@ -1,21 +1,16 @@
-from server.federation import Federation
-
-if __name__ == '__main__':
-    from federation import Federation
-
 class Server:
     def __init__(self):
         pass
     
     def create_federation(self):
-        self.federation = Federation()
+        pass
 
     def print_federation(self):
-        print(self.federation)
+        pass
 
     def train_federation(self, n_rounds):
-        self.federation.train_federation_epoch(n_rounds)
+        pass
     
     def get_results(self):
-        return str(self.federation.results)
+        pass
         
